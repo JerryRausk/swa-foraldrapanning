6cc08ea8ade8ede40027e47983cfc964467b4bdc
Jerry Rausk <Jerry.Rausk@gmail.com>
2023-02-04 21:17:13 +0100

    Add API

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,8 @@
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
+<script lang="ts" setup>
 import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
 </script>
 
 <style>
