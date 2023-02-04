63e11ab88cc7ef62deed91cfa412b7333ef5a412
Jerry Rausk <Jerry.Rausk@gmail.com>
2023-02-04 20:37:48 +0100

    migrate to Vue 3 and TS

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
   name: "App",
-  data() {
-    return {
-      value: "World"
-    };
-  }
-};
+  components: {
+    HelloWorld,
+  },
+});
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
